634f318c99f2280d5d4ccb04dbadd5c5c95cd4a2
Kaitlyn Fernelius <kkin001@gmail.com>
2023-02-26 16:55:25 -0600

    Learned about ngIf and ngFor

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Room, RoomList } from './rooms';
 
 @Component({
   selector: 'hinv-rooms',
@@ -7,12 +8,44 @@ import { Component } from '@angular/core';
 })
 export class RoomsComponent {
   hotelName = 'Hilton Hotel';
-  numberOfRooms = 10;
 
   hideRooms = false;
 
+  room: Room = {
+    totalRooms: 10,
+    availableRooms: 5,
+    bookedRooms: 5
+  }
 
-  toggle(){
+  roomList: RoomList[] = [{
+    roomNumber: 1,
+    roomType: 'Deluxe Room',
+    amenities: 'AirConditioner, Free Wifi, TV, Bathroom',
+    price: 1500,
+    photos: 'https://unsplash.com/photos/Jk6oSN8wSlo',
+    checkInTime: new Date('11-Nov-2021'),
+    checkOutTime: new Date('12-Nov-2021')
+  }, {
+    roomNumber: 2, 
+    roomType: 'Basic Room',
+    amenities: 'AirConditioner, Free Wifi, TV, Bathroom',
+    price: 500,
+    photos: 'https://unsplash.com/photos/Jk6oSN8wSlo',
+    checkInTime: new Date('11-Nov-2021'),
+    checkOutTime: new Date('12-Nov-2021')
+  }, {
+    roomNumber: 3,
+    roomType: 'BasicDeluxe Room',
+    amenities: 'AirConditioner, Free Wifi, TV, Bathroom',
+    price: 550,
+    photos: 'https://unsplash.com/photos/Jk6oSN8wSlo',
+    checkInTime: new Date('11-Nov-2021'),
+    checkOutTime: new Date('12-Nov-2021')
+  }]
+
+  numberOfRooms = this.room.totalRooms;
+
+  toggle() {
     this.hideRooms = !this.hideRooms;
   }
 }
